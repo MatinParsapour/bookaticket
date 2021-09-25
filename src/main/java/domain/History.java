c605069368b931105032e5635bcd7a9d91552604
Matin <matin.parsapour.iam@gmail.com>
2021-09-25 19:20:38 +0330

    update History entity

--- src/main/java/domain/History.java
+++ src/main/java/domain/History.java
@@ -1,12 +1,20 @@
 package domain;
 
 import base.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.List;
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class History extends BaseEntity<Long> {
 
 

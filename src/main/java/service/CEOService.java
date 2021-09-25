04a4b480e6feeae1873b40da63759451860a1f62
Matin <matin.parsapour.iam@gmail.com>
2021-09-25 13:30:45 +0330

    create service for entities

---
+++ src/main/java/service/CEOService.java
@@ -0,0 +1,7 @@
+package service;
+
+import base.service.BaseService;
+import domain.CEO;
+
+public interface CEOService extends BaseService<CEO, Long> {
+}
---
+++ src/main/java/service/CompanyService.java
@@ -0,0 +1,7 @@
+package service;
+
+import base.service.BaseService;
+import domain.Company;
+
+public interface CompanyService extends BaseService<Company, Long> {
+}

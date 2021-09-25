057a5ce24104c838c22e26de2a29d6634e73e84c
Matin <matin.parsapour.iam@gmail.com>
2021-09-25 14:56:54 +0330

    create DemonstrateMenus

---
+++ src/main/java/util/DemonstrateMenus.java
@@ -0,0 +1,60 @@
+package util;
+
+public class DemonstrateMenus {
+    public void CEOMainMenu(){
+        System.out.println("+------------------------------+");
+        System.out.println("|         1.Requests           |");//TODO - CEO main menu - Requests
+        System.out.println("|           2.Exit             |");
+        System.out.println("+------------------------------+");
+    }
+
+    public void employeeMainMenu(){
+        System.out.println("+------------------------------+");
+        System.out.println("|       1.Add a ticket         |");//TODO - Employee main menu - Add a ticket
+        System.out.println("|           2.Exit             |");
+        System.out.println("+------------------------------+");
+    }
+
+    public void employeeFirstMenu(){
+        System.out.println("+------------------------------+");
+        System.out.println("|           1.Enter            |");//TODO - Employee first menu - Enter
+        System.out.println("|        2.Application         |");//TODO - Employee first menu - Application
+        System.out.println("|       3.Your requests        |");//TODO - Employee first menu - User requests
+        System.out.println("|            4.Exit            |");
+        System.out.println("+------------------------------+");
+    }
+
+    public void customerFirstMenu(){
+        System.out.println("+------------------------------+");
+        System.out.println("|          1.Enter             |");//TODO - Customer first menu - Enter
+        System.out.println("|       2.Fill the form        |");//TODO - Customer first menu - Fill the form
+        System.out.println("|           3.Exit             |");
+        System.out.println("+------------------------------+");
+    }
+    public void customerMainMenu(){
+        System.out.println("+------------------------------+");
+        System.out.println("|        1.Your cards          |");//TODO - Customer main menu - Your cards
+        System.out.println("|       2.Book a ticket        |");//TODO - Customer main menu - book a ticket
+        System.out.println("|      3.booked tickets        |");//TODO - Customer main menu - booked tickets
+        System.out.println("|          4.Exit              |");
+        System.out.println("+------------------------------+");
+    }
+
+    public void customerCardsMenu(){
+        System.out.println("+------------------------------+");
+        System.out.println("|        1.Add a card          |");//TODO - Cards - Add a card
+        System.out.println("|       2.Delete a card        |");//TODO - Cards - Delete a card
+        System.out.println("|      3.Change password       |");//TODO - Cards - Change password
+        System.out.println("|     4.Increase balance       |");//TODO - Cards - increase balance
+        System.out.println("|           5.Back             |");
+        System.out.println("+------------------------------+");
+    }
+
+    public void customerTicketsMenu(){
+        System.out.println("+------------------------------+");
+        System.out.println("|       1.Delete booked        |");//TODO - Tickets - delete booked
+        System.out.println("|         2.Buy booked         |");//TODO - Tickets - buy booked
+        System.out.println("|            3.Back            |");
+        System.out.println("+------------------------------+");
+    }
+}

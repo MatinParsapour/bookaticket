45d0667428f5425a13b29c16885a437fcc26ebac
Matin <matin.parsapour.iam@gmail.com>
2021-09-26 07:41:17 +0330

    update Main

--- src/main/java/BookTicket.java
+++ src/main/java/BookTicket.java
@@ -1,2 +1,29 @@
+import util.ApplicationContext;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class BookTicket {
+    public static void main(String[] args) {
+        while(true){
+            try{
+                ApplicationContext.getDemonstrationMenus().mainMenu();
+                int choice = new Scanner(System.in).nextInt();
+                if(choice == 1){
+                    //TODO CEO
+                }else if (choice == 2){
+                    //TODO Employee
+                }else if (choice == 3){
+                    //TODO Customer
+                }else if(choice == 4){
+                    System.out.println("Hope to see you soon");
+                    break;
+                }else{
+                    System.out.println("Wrong input");
+                }
+            }catch (InputMismatchException exception){
+                System.out.println("Wrong input");
+            }
+        }
+    }
 }

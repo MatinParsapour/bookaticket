442e30a1033e35c3cd51b4c5b7dded25e60b0593
Matin <matin.parsapour.iam@gmail.com>
2021-09-26 07:55:01 +0330

    update CustomerRepository

--- src/main/java/repository/CustomerRepository.java
+++ src/main/java/repository/CustomerRepository.java
@@ -4,4 +4,9 @@ import base.repository.BaseRepository;
 import domain.Customer;
 
 public interface CustomerRepository extends BaseRepository<Customer, Long> {
+    Customer findUserByUserNameAndPassword(String userName, String password);
+
+    Customer findCustomerByNationalCode(String nationalCode);
+
+    Customer findCustomerByUserName(String userName);
 }

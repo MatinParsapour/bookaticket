2e439a0d1932056c8b2e57e7de3acfc5e154190b
Matin <matin.parsapour.iam@gmail.com>
2021-09-26 13:29:42 +0330

    create a method to find company by name

--- src/main/java/repository/impl/CompanyRepositoryImpl.java
+++ src/main/java/repository/impl/CompanyRepositoryImpl.java
@@ -2,9 +2,11 @@ package repository.impl;
 
 import base.repository.BaseRepositoryImpl;
 import domain.Company;
+import domain.Employee;
 import repository.CompanyRepository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 public class CompanyRepositoryImpl extends BaseRepositoryImpl<Company, Long> implements CompanyRepository {
     public CompanyRepositoryImpl(EntityManager entityManager) {
@@ -14,5 +16,18 @@ public class CompanyRepositoryImpl extends BaseRepositoryImpl<Company, Long> imp
     @Override
     public Class<Company> getEntity() {
         return Company.class;
+    }
+
+    @Override
+    public Company findCompanyByName(String companyName) {
+        try{
+            return entityManager.createQuery("SELECT c " +
+                    "FROM Company c " +
+                    "WHERE c.companyName = :companyName", Company.class).
+                    setParameter("companyName",companyName).
+                    getSingleResult();
+        }catch (NoResultException exception){
+            return null;
+        }
     }
 }

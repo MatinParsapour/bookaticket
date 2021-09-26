9b7e114569cc11a558dec5e64c0f7cbc97c9939a
Matin <matin.parsapour.iam@gmail.com>
2021-09-26 07:55:30 +0330

    update CustomerService

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -4,4 +4,12 @@ import base.service.BaseService;
 import domain.Customer;
 
 public interface CustomerService extends BaseService<Customer, Long> {
+
+    void firstMenu();
+
+    void fillTheForm();
+
+    void enter();
+
+    void customerMenu();
 }

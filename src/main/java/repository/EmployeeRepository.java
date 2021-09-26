59a7ca4ed8c5283ef89f5f5eb1cae5c52c146562
Matin <matin.parsapour.iam@gmail.com>
2021-09-26 08:57:54 +0330

    enter name of methods for validate employee

--- src/main/java/repository/EmployeeRepository.java
+++ src/main/java/repository/EmployeeRepository.java
@@ -4,4 +4,7 @@ import base.repository.BaseRepository;
 import domain.Employee;
 
 public interface EmployeeRepository extends BaseRepository<Employee, Long> {
+    Employee findEmployeeByNationalCode(String nationalCode);
+
+    Employee findEmployeeByEmployeeCode(long employeeCode);
 }

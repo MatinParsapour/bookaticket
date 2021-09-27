697fac58d0f45ea1df8dd7fa1c6535d7e849e70c
Matin <matin.parsapour.iam@gmail.com>
2021-09-27 10:23:52 +0330

    update HistoryService and HistoryServiceImpl now if customer book a ticket,The ticket will add to history

--- src/main/java/service/HistoryService.java
+++ src/main/java/service/HistoryService.java
@@ -2,6 +2,9 @@ package service;
 
 import base.service.BaseService;
 import domain.History;
+import domain.Ticket;
 
 public interface HistoryService extends BaseService<History,Long> {
+
+    void addHistory(Ticket ticket, int passengers);
 }
--- src/main/java/service/impl/HistoryServiceImpl.java
+++ src/main/java/service/impl/HistoryServiceImpl.java
@@ -2,11 +2,19 @@ package service.impl;
 
 import base.service.BaseServiceImpl;
 import domain.History;
+import domain.Ticket;
 import repository.HistoryRepository;
 import service.HistoryService;
+import util.SecurityUser;
 
 public class HistoryServiceImpl extends BaseServiceImpl<History,Long, HistoryRepository> implements HistoryService {
     public HistoryServiceImpl(HistoryRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public void addHistory(Ticket ticket, int passengers) {
+        History history = new History(SecurityUser.getCustomer(),ticket,passengers,true,false);
+        createOrUpdate(history);
     }
 }
